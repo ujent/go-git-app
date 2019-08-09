0596b54ce795bc85240cce21e8a8d749e4a03603
ujent <ujent2006@mail.ru>
2019-08-10 00:53:34 +0700

    Add test for getting repositories

---
+++ gitSvc/service_test.go
@@ -0,0 +1,47 @@
+package gitSvc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ujent/go-git-app/config"
+	"github.com/ujent/go-git-app/contract"
+	gitsvc "github.com/ujent/go-git-app/gitSvc"
+)
+
+const userName = "test_user"
+const userEmail = "test_user@gmail.com"
+
+func TestRepositories(t *testing.T) {
+	s, err := config.ParseTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	svc, err := gitsvc.New(&contract.Credentials{Name: userName, Email: userEmail}, s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = svc.CreateRepository("repo_1")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = svc.CreateRepository("repo_2")
+	if err != nil {
+		t.Error(err)
+	}
+
+	repos, err := svc.Repositories()
+	if err != nil {
+		t.Error(err)
+	}
+
+	must := 2
+	if len(repos) < must {
+		t.Errorf("Repositories quantity (%d) is less than %d \n", len(repos), must)
+	}
+
+	fmt.Println(repos)
+}
