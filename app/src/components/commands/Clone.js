a5b7b3d72c22a7abe60b973348ccccf9ff3d611c
ujent <ujent2006@mail.ru>
2019-08-28 21:26:44 +0700

    Add clone component

--- app/src/components/commands/Clone.js
+++ app/src/components/commands/Clone.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../App.css';
 
-const Clone = props => {
-    return (
-        <li>
-            <div className="command-block command-block-content">
-                <button type="button" className="button" disabled={!props.isAvailable}>Clone</button>
-                <input type="text" placeholder="URL" className="command-block-input" />
-            </div>
-            <div className="credentials">
-                <input type="text" placeholder="user" />
-                <input type="password" placeholder="password" />
-            </div>
-            <hr></hr>
-        </li>
-    );
-};
+export default class Clone extends Component {
+    constructor(props) {
+        super(props);
 
-export default Clone;
+        this.state = {
+            url: '',
+            name: '',
+            psw: ''
+        }
+    }
+
+    render() {
+        const isAvailable = this.props.isAvailable && this.state.url;
+
+        const onURLChange = (e) => {
+            this.setState({
+                url: e.target.value
+            });
+        }
+
+        const onAuthNameChange = (e) => {
+            this.setState({
+                name: e.target.value
+            });
+        }
+
+        const onAuthPswChange = (e) => {
+            this.setState({
+                psw: e.target.value
+            });
+        }
+
+        const onCloneClick = () => {
+            this.props.action(this.state.url, this.state.name, this.state.psw)
+            this.setState({
+                url: '',
+                name: '',
+                psw: ''
+            });
+        }
+
+        return (
+            < li >
+                <div className="command-block command-block-content">
+                    <button type="button" className="button" disabled={!isAvailable} onClick={() => onCloneClick()}>Clone</button>
+                    <input type="text" placeholder="URL" className="command-block-input" value={this.state.url} onChange={(e) => onURLChange(e)} />
+                </div>
+                <div className="credentials">
+                    <input type="text" placeholder="user" value={this.state.name} onChange={(e) => onAuthNameChange(e)} />
+                    <input type="password" placeholder="password" value={this.state.psw} onChange={(e) => onAuthPswChange(e)} />
+                </div>
+                <hr></hr>
+            </li >
+        );
+
+    };
+};
