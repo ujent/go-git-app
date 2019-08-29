ef066cc1bbdf47e88337728613cb3dd71bfb3e07
ujent <ujent2006@mail.ru>
2019-08-29 10:07:29 +0700

    Add push component

--- app/src/components/commands/Push.js
+++ app/src/components/commands/Push.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../App.css';
 
-const Push = props => {
-    return (
-        <li>
-            <div className="command-block command-block-content">
-                <button type="button" className="button" disabled={!props.isAvailable}>Push</button>
-                <input type="text" placeholder="remote" className="command-block-input" />
-            </div>
-            <div className="credentials">
-                <input type="text" placeholder="user" />
-                <input type="password" placeholder="password" />
-            </div>
-            <hr></hr>
-        </li>
-    );
-}
+export default class Push extends Component {
+    constructor(props) {
+        super(props);
 
-export default Push;
+        this.state = {
+            url: '',
+            name: '',
+            psw: ''
+        }
+    }
+
+    render() {
+        const onURLChange = (e) => {
+            this.setState({
+                url: e.target.value
+            });
+        }
+
+        const onAuthNameChange = (e) => {
+            this.setState({
+                name: e.target.value
+            });
+        }
+
+        const onAuthPswChange = (e) => {
+            this.setState({
+                psw: e.target.value
+            });
+        }
+
+        const onPushClick = () => {
+            this.props.action(this.state.url, this.state.name, this.state.psw);
+            this.setState({
+                url: '',
+                name: '',
+                psw: ''
+            });
+        }
+
+        return (
+            <li>
+                <div className="command-block command-block-content">
+                    <button type="button" className="button" disabled={!this.props.isAvailable} onClick={onPushClick} >Push</button>
+                    <input type="text" placeholder="remote" className="command-block-input" value={this.state.url} onChange={onURLChange} />
+                </div>
+                <div className="credentials">
+                    <input type="text" placeholder="user" value={this.state.name} onChange={onAuthNameChange} />
+                    <input type="password" placeholder="password" value={this.state.psw} onChange={onAuthPswChange} />
+                </div>
+                <hr></hr>
+            </li>
+        );
+    };
+
+}
