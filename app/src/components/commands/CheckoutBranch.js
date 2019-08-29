dcbbf23b277f4ebae23b93262677a4b6775bf712
ujent <ujent2006@mail.ru>
2019-08-29 11:10:24 +0700

    Add checkout branch component

--- app/src/components/commands/CheckoutBranch.js
+++ app/src/components/commands/CheckoutBranch.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../App.css';
 
-const CheckoutBranch = props => {
+export default class CheckoutBranch extends Component {
+    constructor(props) {
+        super(props);
 
-    const branchOptions = props.branches.map(
-        opt => {
-            return (
-                <option key={opt} value={opt}>{opt}</option>
-            );
+        this.state = {
+            branch: ''
         }
-    );
+    }
 
-    return (
-        <li className="command-block">
-            <div className="command-block-content">
-                <button type="button" className="button medium-button" disabled={!props.isAvailable}>Checkout branch</button>
-                <select placeholder="select" className="command-block-input" defaultValue="">
-                    <option value="" disabled hidden>select</option>
-                    {branchOptions}
-                </select>
-            </div>
-            <hr></hr>
-        </li>
-    );
+    render() {
+        const isAvailable = this.props.isAvailable && this.state.branch;
+
+        const branchOptions = this.props.branches.map(
+            opt => {
+                return (
+                    <option key={opt} value={opt}>{opt}</option>
+                );
+            }
+        );
+
+        const onCheckoutClick = () => {
+            this.props.action(this.state.branch);
+
+            this.setState({
+                branch: ''
+            });
+        }
+
+        const onBranchChange = (e) => {
+            this.setState({
+                branch: e.target.value
+            });
+        }
+
+        return (
+            <li className="command-block">
+                <div className="command-block-content">
+                    <button type="button" className="button medium-button" disabled={!isAvailable} onClick={onCheckoutClick} >Checkout branch</button>
+                    <select placeholder="select" className="command-block-input" value={this.state.branch} onChange={onBranchChange} >
+                        <option value="" disabled hidden>select</option>
+                        {branchOptions}
+                    </select>
+                </div>
+                <hr></hr>
+            </li>
+        );
+    }
+
 }
-
-export default CheckoutBranch;
